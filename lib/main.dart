c674213d3eacb1e50cbe7d999a78a7ecbcf5070d
xanf-code <darshanaswath@gmail.com>
2021-03-15 00:10:43 +0530

    Initial Portfolio

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'my_app.dart';
 
 void main() {
-  // ignore: invalid_use_of_visible_for_testing_member
   runApp(MyApp());
 }
